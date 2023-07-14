7631f9d5011d4e675797250187b8a129b7cdf3a3
Lalit.Kumar <lalitchoudhary503@gmail.com>
2023-07-14 11:48:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+let name = 'lalit';
+let number = 30;
+let isAvailabe = true;
+let fName = undefined;
+let lName = null;
+
+const pie = 22;
+//pie = 34;
+
+console.log(pie);
+
+//Assignment operator
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2", comp1 == comp2); // checks only value 
+console.log("comp1 === comp2", comp1 === comp2); // checks value and type as well 
+
+// ternary operator
+let ter = 12;
+console.log(ter == 10 ? "is 10" : "not 10");
+
+
+
+//user defined 
+//object
+//array
+
+let person = {
+    //key & value
+    pName : 'lalit',
+    pAge : 30,
+    pAddress : {
+        street : '134',
+        landmark : 'test'
+    } 
+};
+
+
+let arr = ['lalit', 'kumar', 123];
+
+console.log(arr.length);
+
+
+function myFunction(a , b){
+   return a * b;
+}
+
+console.log(myFunction(3, 4));
+
+//variable user defined (object & array)
+//variable primitive (let & const)
+//functions
+
+mylabda = (a) => a + 100;
+
+// lambda function 
+// anonymous function  
+
+
+
+
+
+
+
