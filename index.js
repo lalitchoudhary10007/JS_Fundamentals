963fd697f4e854cd7c82926fdecb5130968d9c5c
Lalit.Kumar <lalitchoudhary503@gmail.com>
2023-07-24 19:00:37 +0530

    window, dom, bom, node, element navigation

--- index.js
+++ index.js
@@ -35,6 +35,50 @@ let person = {
     } 
 };
 
+//for in loop
+for (const key in person) {
+    if (Object.hasOwnProperty.call(person, key)) { //check key in object
+        const element = person[key];
+        console.log("key ", key + " element / value "+element);    
+    }
+}
+
+//for of loop
+for (const iterator of "Lalit") {
+    console.log("iterator ", iterator);
+}
+
+//Template literals
+let a = 'Abc';
+let x = 'XYZ';
+console.log("literals use ", `${a} is opposite to ${x}`); // after using ${} will pick value of variable
+
+//Higher order functions map,reduce,filter
+let array1 = [45,23,21,22,66];
+//for each
+array1.forEach((value) => {
+   console.log("for each :- ", value);
+});
+//map method
+let mapArray = array1.map((value, index, array) => {
+   return value + 1;
+});
+console.log("Map array :- ", mapArray);
+//difference between map & for each is map creates a new array of returned values and foreach just returns value
+
+let filterArray = [5, 6, 7, 8, 9, 4, 3, 2];
+let fArray = filterArray.filter((value) => {
+    return value > 5;
+});
+console.log("Filtered Array:- ", fArray);
+
+//Reduce method
+let arr3 = [1,2,3,4];
+let arry3 = arr3.reduce((v1, v2)=> {
+    return v1 + v2;
+});
+console.log("Reduce array :- ", arry3);
+
 
 let arr = ['lalit', 'kumar', 123];
 
@@ -45,16 +89,30 @@ function myFunction(a , b){
    return a * b;
 }
 
-console.log(myFunction(3, 4));
+//window is a global object
+//bom - browser object model
+//dom - document object model, complete html page javascript object
+console.log(document.body);
+//additional objects provided by host environment
+//alert confirm prompt etc. are bom objects
 
-//variable user defined (object & array)
-//variable primitive (let & const)
-//functions
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
 
-mylabda = (a) => a + 100;
+//parent node & parent element 
+console.log(document.documentElement.parentElement) // return null because there is not parent element of html element
+console.log(document.documentElement.parentNode) //return document because document node is availabe of html element
+//documentElement = html element
 
-// lambda function 
-// anonymous function  
+
+//element only naviagtion
+console.log(document.body.firstChild)//return first child can be node(text, comment etc.) and element 
+console.log(document.body.firstElementChild)//return first element only will ignore nodes
+
+
+
+
 
 
 
